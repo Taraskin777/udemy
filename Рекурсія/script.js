cba077d8a1db26fc0ecbd4bf5f0ce4e8858885e5
Taras Noga <taraskin2012@gmail.com>
2022-05-08 23:23:46 +0300

    add example with recursion method

--- Рекурсія/script.js
+++ Рекурсія/script.js
@@ -41,28 +41,53 @@ let students = {
   },
 };
 
-function getTotalProgressByIteration(data) {
-  let total = 0;
-  let students = 0;
+// function getTotalProgressByIteration(data) {
+//   let total = 0;
+//   let students = 0;
 
-  for (let course of Object.values(data)) {
-    if (Array.isArray(course)) {
-      students += course.length;
-      for (let i = 0; i < course.length; i++) {
-        total += course[i].progress;
-      }
-    } else {
-      for (let subCourse of Object.values(course)) {
-         students += subCourse.length;
-         
-        for (let i = 0; i < subCourse.length; i++) {
-          total += subCourse[i].progress;
-        }
-      }
+//   for (let course of Object.values(data)) {
+//     if (Array.isArray(course)) {
+//       students += course.length;
+//       for (let i = 0; i < course.length; i++) {
+//         total += course[i].progress;
+//       }
+//     } else {
+//       for (let subCourse of Object.values(course)) {
+//          students += subCourse.length;
+
+//         for (let i = 0; i < subCourse.length; i++) {
+//           total += subCourse[i].progress;
+//         }
+//       }
+//     }
+//   }
+
+//   return total / students;
+// }
+
+// console.log(getTotalProgressByIteration(students));
+
+function getTotalProgressByRecursion(data) {
+  if (Array.isArray(data)) {
+    let total = 0;
+    for (let i = 0; i < data.length; i++) {
+      total += data[i].progress;
     }
-  }
 
-  return total / students;
+    return [total, data.length];
+  } else {
+    let total = [0, 0];
+
+    for (let subData of Object.values(data)) {
+      const subDataArray = getTotalProgressByRecursion(subData);
+      total[0] += subDataArray[0];
+      total[1] += subDataArray[1];
+    }
+
+    return total;
+  }
 }
 
-console.log(getTotalProgressByIteration(students));
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0] / result[1]);
